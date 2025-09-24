129f7fb8acbbc3e1d6ae0b9ec4e27f1561c1a974
praja09 <dipaprajadpp@gmail.com>
2025-09-24 11:47:18 +0700

    Jobsheet 3_Tambah data dummy dan tampilkan riwayat

--- bin/kalkulator_bmi.dart
+++ bin/kalkulator_bmi.dart
@@ -1,3 +1,11 @@
+void main() {
+  List<Map<String, dynamic>> riwayat = [];
+  hitungBMI(170, 65, riwayat);
+  hitungBMI(165, 75, riwayat);
+  hitungBMI(180, 85, riwayat);
+  tampilkanRiwayat(riwayat);
+}
+
 void hitungBMI(double tinggiCm, double beratKg, List<Map<String, dynamic>> riwayat) {
   if (tinggiCm <= 0 || beratKg <= 0) {
       throw Exception("Tinggi dan berat harus lebih dari 0");
